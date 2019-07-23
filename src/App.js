c886453339a92fa86e142307bad2ef36538cc78d
Francisco Plaza Urrutia <fpu61794@dominio.es>
2019-07-23 14:56:22 +0200

    Making design

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const Pad = (props) => {
+  return(
+    <div className='drum-pad'>
+      {props.param}
     </div>
   );
 }
+
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      keyPressed : ''
+    };
+  }
+  
+  
+  render() {
+    return(
+      <div className="App">
+        <div className="Grid" id="drum-machine">
+            <Pad param='Q'></Pad>
+            <Pad param='W'></Pad>
+            <Pad param='E'></Pad>
+            <Pad param='A'></Pad>
+            <Pad param='S'></Pad>
+            <Pad param='D'></Pad>
+            <Pad param='Z'></Pad>
+            <Pad param='X'></Pad>
+            <Pad param='C'></Pad>
+          </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
 
 export default App;
