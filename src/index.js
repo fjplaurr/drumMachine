b5f1f045ac0467ff3237f591ad2e7fecb8b634a3
fjplaurr <fjplaurr@gmail.com>
2019-10-15 21:31:12 +0200

    index modified

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/store'
+
+const root = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(root, document.getElementById('root'));
