803577dd2a7a5ad04a1ef28746f065f4c7d41a04
Francisco Plaza Urrutia <fplaza@CSP.local>
2019-12-16 12:50:00 +0100

    Updates

---
+++ precache-manifest.120d95cfed4eed67ea9a5c6f91930f12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95fd0f923ce68e7e58f7d4bcf86f084",
+    "url": "/drumMachine/index.html"
+  },
+  {
+    "revision": "b5ba66126c08f0c6f847",
+    "url": "/drumMachine/static/css/main.845e8142.chunk.css"
+  },
+  {
+    "revision": "44358564e37fc58dec5e",
+    "url": "/drumMachine/static/js/2.679238c9.chunk.js"
+  },
+  {
+    "revision": "b5ba66126c08f0c6f847",
+    "url": "/drumMachine/static/js/main.9f9fe946.chunk.js"
+  },
+  {
+    "revision": "586fd1d697d60033611f",
+    "url": "/drumMachine/static/js/runtime~main.3110db73.js"
+  }
+]);
